d401fd91ea3036f963fc7c812869a1e3f2830ac7
16gsanket <16gsanket@gmail.com>
2024-05-06 20:43:31 +0530

    added to v1

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 
 export default function NavBar() {
   return (
-    <div className=' h-[15dvh]  flex align-middle justify-between md:px-10 sm:px-8 px-5 items-center '>
+    <div className=' h-[15dvh]  flex align-middle justify-between md:px-10 sm:px-8 px-5 items-center font-semibold'>
        
             <ul>
                 SG
             </ul>
 
-            <ul className='flex align-middle justify-between gap-4'>
+            <ul className='flex align-middle justify-between gap-4 w-6/12'>
                 <li>Home</li>
                 <li>Skills</li>
                 <li>Projects</li>
                 <li>Blogs</li>
+                <ul className='flex align-middle justify-between'>
+                    <li>Li</li>
+                    <li>Ig</li>
+                </ul>
             </ul>
         
     </div>
--- src/Pages/Page2.jsx
+++ src/Pages/Page2.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function() {
   return (
-    <div className='h-screen w-full bg-slate-950'></div>
+    <div className='min-h-screen w-full bg-bk_2_black flex flex-col align-middle justify-around items-center'>
+        <div className=' h-fit w-4/12 text-center'>
+            <h2 className='text-xl font-semibold text-orange_text'>SERVICES</h2>
+            <h1 className='text-4xl font-semibold'>What I am Great At</h1>
+            <p className='text-md'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat culpa harum atque nisi ea repellendus?</p>
+        </div>
+        <div className='h-40vh w-full bg-red-400'>
+            <h1>hello</h1>
+        </div>
+    </div>
   )
 }
