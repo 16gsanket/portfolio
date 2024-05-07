22fed49165c80ca085cd78d97429b50038cc1284
16gsanket <16gsanket@gmail.com>
2024-05-07 22:02:28 +0530

    added projects to page 3

--- src/Pages/Page3.jsx
+++ src/Pages/Page3.jsx
@@ -1,6 +1,95 @@
+/*
+full -> full Stack Apps
+front -> Front End Apps
+xi -> ui/ux
+*/
+
+import { useState } from "react";
+
+const PROJECTS = [
+  {
+    tittle: "The Wild Oasis",
+    description:
+      "This project simplifies internal hotel operations by providing a user-friendly platform for staff members. With this system, staff can efficiently manage guest check-in/out, breakfast options, room bookings, and other hotel-related tasks. By centralizing these functions, the project enhances staff productivity and ensures a seamless experience for both employees and guests",
+    tech_stack: ["react , react-router , context-API , supabase, react-charts"],
+    deployment_link: "https://the-wild-oasis-ashen-alpha.vercel.app/",
+    github_link: "https://github.com/16gsanket/the-wild-oasis",
+    type: "full",
+  },
+  {
+    tittle: "Property Broker",
+    description:
+      "The Property Broker App offers a streamlined solution for real estate agencies, digitizing their operations and enhancing customer convenience. Designed to simplify property searches, this user-friendly app provides easy access to listings, revolutionizing the way clients find their ideal properties.",
+    tech_stack: [
+      "react",
+      "react-router@6",
+      "React Hook Form",
+      "React Redux Toolkit",
+      "supabase",
+      "react-charts",
+    ],
+    deployment_link: "https://property-broker-one.vercel.app/",
+    github_link: "https://github.com/16gsanket/PropertyBroker",
+    type: "full",
+  },
+  {
+    tittle: "Fast React Pizza Co",
+    description:
+      "A Application Catering to the business need of the Pizza Company , for the users to order pizza where we already have a ready made API",
+    tech_stack: ["react", "react-router", "React Redux Toolkit,"],
+    deployment_link: "https://fast-react-pizza-gules.vercel.app/",
+    github_link: "https://github.com/16gsanket/fast-react-pizza",
+    type: "front",
+  },
+  {
+    tittle: "World Wise App",
+    description:
+      "A Application for keeping a track of the location where you go .. bifercating your locations into cities and countries",
+    tech_stack: ["react", "react-router"],
+    deployment_link: "https://world-wide-delta.vercel.app/",
+    github_link: "https://github.com/16gsanket/WorldWide",
+    type: "front",
+  },
+  // https://snlet.github.io/SpainCollectionClone/
+  {
+    tittle: "Spain Collection",
+    description: "A travel website for the advertisment of Spain ",
+    tech_stack: ["html", "css", "javascript", "GSAP", "Scroll-Trigger"],
+    deployment_link: "https://snlet.github.io/SpainCollectionClone/",
+    github_link: "https://github.com/Snlet/SpainCollectionClone/",
+    type: "xi",
+  },
+  {
+    tittle: "Wednesday",
+    description: "A travel website for the advertisment of Spain ",
+    tech_stack: ["html", "css", "javascript", "GSAP", "Scroll-Trigger"],
+    deployment_link: "https://snlet.github.io/Wednesday/",
+    github_link: "https://github.com/Snlet/Wednesday/",
+    type: "xi",
+  },
+];
+
 function Page3() {
+  const [typeHolder, setTypeHolder] = useState("all");
+
+  let filtered_list = [];
+
+  if (typeHolder === "all") filtered_list = PROJECTS;
+
+  if (typeHolder === "full") {
+    filtered_list = PROJECTS.filter((project) => project.type === "full");
+  }
+  if (typeHolder === "front") {
+    filtered_list = PROJECTS.filter((project) => project.type === "front");
+  }
+  if (typeHolder === "xi") {
+    filtered_list = PROJECTS.filter((project) => project.type === "xi");
+  }
+
+  console.log(filtered_list);
+
   return (
-    <div className="h-[90dvh] w-full bg-bk_black flex flex-col gap-2 align-middle justify-around items-center ">
+    <div className="h-[120dvh] w-full bg-bk_black flex flex-col gap-2 align-middle justify-around items-center ">
       <div className="w-8/12 h-fit py -5  text-center flex flex-col gap-4">
         <h2 className="text-md text-orange_text font-semibold tracking-wider ">
           PORTFOLIO
@@ -14,17 +103,33 @@ function Page3() {
           proident, sunt in culpa qui officia
         </p>
       </div>
-      <div className="h-4/6 w-10/12  flex flex-col gap-1">
+      <div className="h-4/6 w-10/12  flex flex-col gap-10">
         <div className="h-1/6 w-full text-senter p-2">
           <ul className=" flex align-middle justify-center items-center">
-            <button className="sections font-bold">ALL</button>
-            <button className="sections font-bold">
+            <button
+              className="sections font-bold"
+              onClick={() => setTypeHolder("all")}
+            >
+              ALL
+            </button>
+            <button
+              className="sections font-bold"
+              onClick={() => setTypeHolder("front")}
+            >
               FRONTEND <br /> DEVELOPMENT
             </button>
-            <button className="sections font-bold">
+            <button
+              className="sections font-bold"
+              onClick={() => setTypeHolder("full")}
+            >
               BACKEND <br /> DEVELOPMENT
             </button>
-            <button className="sections font-bold">UI/UX</button>
+            <button
+              className="sections font-bold"
+              onClick={() => setTypeHolder("xi")}
+            >
+              UI/UX
+            </button>
           </ul>
         </div>
         <div className="h-5/6 w-full bg-green-400"></div>
--- src/Pages/Page4.jsx
+++ src/Pages/Page4.jsx
@@ -2,14 +2,19 @@ const ONGOING_PRESENT_PROJECT = [
   {
     date: "21 April 2024",
     title: "The Wild Oasis",
-    information:"Cured with care for internal staff use only of the hotel of the oasisi hotel using the supabase ofr backend",
+    information:
+      "Cured with care for internal staff use only of the hotel of the oasisi hotel using the supabase ofr backend",
     stack: "React, React Router@4 ",
+    link: "",
+    github: "https://github.com/16gsanket/the-wild-oasis",
   },
   {
     date: "21 April 2024",
-    title: "The Wild Oasis",
-    information:"For Real Estate",
+    title: "real Estate Pro",
+    information: "For Real Estate",
     stack: "React, React Router@4 ",
+    link: "https://property-broker-one.vercel.app/",
+    github: "https://github.com/16gsanket/PropertyBroker",
   },
 ];
 
@@ -31,37 +36,32 @@ function Page4() {
       </div>
       <div className=""></div>
       <div className="h-fit w-full md:flex">
-        {ONGOING_PRESENT_PROJECT.map(project=>
-            
-        <div class="bg-bk_black shadow-[0_8px_12px_-6px_rgba(0,0,0,0.2)]  p-4 w-full max-w-sm rounded-lg font-[sans-serif] overflow-hidden mx-auto mt-4">
-          <img
-            src="https://readymadeui.com/cardImg.webp"
-            class="w-full rounded-lg"
-          />
-          <div class="px-4 my-6 text-center">
-            <h3 class="text-lg font-semibold">{project.title}</h3>
-            <p class="mt-2 text-sm text-gray-400">
-              {project.information}
-            </p>
-            <button
-              type="button"
-              class="px-6 py-2 w-5/12 mt-4 mx-4 rounded-lg text-white text-sm tracking-wider font-semibold border-none outline-none bg-orange_text hover:bg-orange_hover active:bg-blue-600"
-            >
-              Visit
-            </button>
-            <button
-              type="button"
-              class="px-6 py-2 w-5/12 mt-4 rounded-lg text-white text-sm tracking-wider font-semibold border outline-none  hover:bg-stone-500 active:bg-blue-600"
-            >
-              GitHub
-            </button>
+        {ONGOING_PRESENT_PROJECT.map((project) => (
+          <div class="bg-bk_black shadow-[0_8px_12px_-6px_rgba(0,0,0,0.2)]  p-4 w-full max-w-sm rounded-lg font-[sans-serif] overflow-hidden mx-auto mt-4">
+            <img
+              src="https://readymadeui.com/cardImg.webp"
+              class="w-full rounded-lg"
+            />
+            <div class="px-4 my-6 text-center">
+              <h3 class="text-lg font-semibold">{project.title}</h3>
+              <p class="mt-2 text-sm text-gray-400">{project.information}</p>
+              <button
+                type="button"
+                class="px-6 py-2 w-5/12 mt-4 mx-4 rounded-lg text-white text-sm tracking-wider font-semibold border-none outline-none bg-orange_text hover:bg-orange_hover active:bg-blue-600"
+              >
+                Visit
+              </button>
+
+              <button
+                type="button"
+                class="px-6 py-2 w-5/12 mt-4 rounded-lg text-white text-sm tracking-wider font-semibold border outline-none  hover:bg-stone-500 active:bg-blue-600"
+              >
+                GitHub
+              </button>
+            </div>
           </div>
-        </div>
-        )}
-
-
+        ))}
       </div>
-    
     </div>
   );
 }
